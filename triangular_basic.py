d5487f60a95f5303a5ebbfe4b2e1163cd3733d35
ehens86 <eghensley@email.wm.edu>
2019-03-10 21:46:30 -0400

    tri

--- triangular_basic.py
+++ triangular_basic.py
@@ -10,39 +10,133 @@ sys.path.insert(1, os.path.join(cur_path, 'lib', 'python3.7', 'site-packages'))
 
 
 from update_psql import pg_query
-from _connections import db_connection
+from _connections import db_connection, binance_connection
 import operator
+import time
 
 PSQL = db_connection('psql')
-
-eth_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
-	from binance.exchanges ex \
-	join binance.markets mk on mk.market_id = ex_market_id \
-	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
-		and buy_coin = 'ETH'"
-eth_prices = pg_query(PSQL.client, eth_script)
-eth_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
-eth_conv = {k:float(v) for k,v in eth_prices[['sell_coin', 'open']].values}
-
-btc_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
-	from binance.exchanges ex \
-	join binance.markets mk on mk.market_id = ex_market_id \
-	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
-		and buy_coin = 'BTC'"
-btc_prices = pg_query(PSQL.client, btc_script)
-btc_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
-btc_conv = {k:float(v) for k,v in btc_prices[['sell_coin', 'open']].values}
-
-eth_btc = btc_conv['ETH']
-btc_conv.pop('ETH')
+BINANCE = binance_connection()
 
 
+def cap_market(coin_name):
+    coineth_depth = BINANCE.get_order_book(symbol='%sETH' % (coin_name))
+    coinbtc_depth = BINANCE.get_order_book(symbol='%sBTC' % (coin_name))
+    ethbtc_depth = BINANCE.get_order_book(symbol='ETHBTC')
+    
+    buy_prices = {}
+    buy_prices[coin_name] = {'ETH': {'price': [float(coineth_depth['asks'][0][0]), float(coineth_depth['asks'][1][0])], 'volume': [float(coineth_depth['asks'][0][1]), float(coineth_depth['asks'][1][1])]}}
+    buy_prices[coin_name]['BTC'] = {'price': [float(coinbtc_depth['asks'][0][0]), float(coinbtc_depth['asks'][1][0])], 'volume': [float(coinbtc_depth['asks'][0][1]), float(coinbtc_depth['asks'][1][1])]}
+    buy_prices['ETH'] = {'BTC': {'price': [float(ethbtc_depth['asks'][0][0]), float(ethbtc_depth['asks'][1][0])], 'volume': [float(ethbtc_depth['asks'][0][1]), float(ethbtc_depth['asks'][1][1])]}}
 
-traingle = {}
-for coin_2_name, coin_2_eth_price in eth_conv.items():
-    if coin_2_name in btc_conv.keys():
-        coin_2_btc_price = btc_conv[coin_2_name]
-        arb = ((1/coin_2_eth_price)*coin_2_btc_price)/eth_btc
-        traingle[coin_2_name] = arb
+    buy_prices['ETH'][coin_name] = {'price': [1/float(coineth_depth['bids'][0][0]), 1/float(coineth_depth['bids'][1][0])], 'volume': [float(coineth_depth['bids'][0][1]), float(coineth_depth['bids'][1][1])]}
+    buy_prices['BTC'] = {'ETH': {'price': [1/float(ethbtc_depth['bids'][0][0]), 1/float(ethbtc_depth['bids'][1][0])], 'volume': [float(ethbtc_depth['bids'][0][1]), float(ethbtc_depth['bids'][1][1])]}}
+    buy_prices['BTC'][coin_name] = {'price': [1/float(coinbtc_depth['bids'][0][0]), 1/float(coinbtc_depth['bids'][1][0])], 'volume': [float(coinbtc_depth['bids'][0][1]), float(coinbtc_depth['bids'][1][1])]}
+
+    return(buy_prices)
         
-sorted(traingle.items(), key=operator.itemgetter(1), reverse = True)
+
+
+
+def sim_tri(exchange_rates, swap_order):
+    """
+    ETH -> coin -> BTC -> ETH
+    """
+    
+#    coin_1, coin_2 = 'HOT', 'BTC'
+#    exchange_rates = prices
+
+    coin_1, coin_2 = swap_order
+
+    try:
+        exchange_hist = [('ETH', 1)]
+        # 1 ETH -> x coin
+        coin_1_from_eth = 1/exchange_rates[coin_1]['ETH']['price'][0]
+        if coin_1_from_eth <= exchange_rates[coin_1]['ETH']['volume'][0]:
+            coin_1_from_eth *= .999
+            exchange_hist.append((coin_1, coin_1_from_eth))
+        else:
+            raise Exception('Illiquid')
+            
+        # x coin -> y BTC
+        coin_2_from_coin_1 = coin_1_from_eth / exchange_rates[coin_2][coin_1]['price'][0]
+        if coin_1_from_eth <= exchange_rates[coin_2][coin_1]['volume'][0]:
+            coin_2_from_coin_1 *= .999
+            exchange_hist.append((coin_2, coin_2_from_coin_1))
+        else:
+            raise Exception('Illiquid')   
+     
+        # y BTC -> z ETH
+        eth_from_coin_2 = coin_2_from_coin_1 / exchange_rates['ETH'][coin_2]['price'][0]
+        if coin_2_from_coin_1 <= exchange_rates['ETH'][coin_2]['volume'][0]:
+            eth_from_coin_2 *= .999
+            exchange_hist.append(('ETH', eth_from_coin_2))
+        else:
+            raise Exception('Illiquid')   
+        
+        return(exchange_hist)
+    except:
+        return(False)
+        
+        
+def filter_potentials(): 
+    eth_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'ETH'"
+    eth_prices = pg_query(PSQL.client, eth_script)
+    eth_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    eth_conv = {k:float(v) for k,v in eth_prices[['sell_coin', 'open']].values}
+    
+    btc_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'BTC'"
+    btc_prices = pg_query(PSQL.client, btc_script)
+    btc_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    btc_conv = {k:float(v) for k,v in btc_prices[['sell_coin', 'open']].values}
+    
+    eth_btc = btc_conv['ETH']
+    btc_conv.pop('ETH')
+    
+    traingle = {}
+    for coin_2_name, coin_2_eth_price in eth_conv.items():
+        if coin_2_name in btc_conv.keys():
+            coin_2_btc_price = btc_conv[coin_2_name]
+            arb = ((1/coin_2_eth_price)*coin_2_btc_price)/eth_btc
+            traingle[coin_2_name] = abs(1 - arb)
+            
+    traingle = sorted(traingle.items(), key=operator.itemgetter(1), reverse = True)
+    _potentials = [i for i in traingle if i[1] >= .01]
+    return(_potentials)
+    
+potentials = filter_potentials()
+
+while True:
+    for coin, _ in potentials:
+        print('Analyzing %s' % (coin))
+        
+        prices = cap_market(coin)
+            
+        forward_tri = sim_tri(prices, ['BTC', coin])
+        if forward_tri and forward_tri[-1][-1] > 1:
+            asdfasdf
+        backward_tri = sim_tri(prices, [coin, 'BTC'])
+        if backward_tri and backward_tri[-1][-1] > 1:
+            asdfasdf        
+    
+    time.sleep(15)
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
--- update_psql.py
+++ update_psql.py
@@ -21,6 +21,7 @@ from pg_tables import create_tables
 import numpy as np
 from progress_bar import progress
 import time as tm
+import operator 
 
 def pg_create_table(cur, table_name):  
 #    cur, table_name = _psql, 
@@ -307,14 +308,259 @@ def pop_prices(psql, time_conv, coins):
             pg_insert(psql.client, script)   
     print('\n')
        
-             
+
+def cap_market(coin_name_1, coin_name_2, binance):
+#    coin_name_1, coin_name_2, binance = coin1, coin2, BINANCE_2
+#    coin_name_2 = 'BNB'
+#    coin_name_3 = 'BTC'
+    
+    coin1eth_depth = binance.get_order_book(symbol='%sETH' % (coin_name_1))
+    coin1coin2_depth = binance.get_order_book(symbol='%s%s' % (coin_name_1, coin_name_2))
+    if coin_name_2 in ['BNB', 'TUSD']:
+        ethcoin2_depth = binance.get_order_book(symbol='%sETH' % (coin_name_2))
+    else:
+        ethcoin2_depth = binance.get_order_book(symbol='ETH%s' % (coin_name_2))
+#    if coin_name_3:
+#        coin2coin3_depth = binance.get_order_book(symbol='%s%s' % (coin_name_2, coin_name_3))
+#        if coin_name_3 in ['BNB', 'TUSD']:
+#            ethcoin3_depth = binance.get_order_book(symbol='%sETH' % (coin_name_3))
+#        else:
+#            ethcoin3_depth = binance.get_order_book(symbol='ETH%s' % (coin_name_3))        
+#        
+    
+    eth_available = binance.get_asset_balance(asset='ETH')
+    
+    buy_prices = {}
+    buy_prices[coin_name_1] = {'ETH': {'price': [float(coin1eth_depth['asks'][0][0]), float(coin1eth_depth['asks'][1][0])], 'volume': [float(coin1eth_depth['asks'][0][1]), float(coin1eth_depth['asks'][1][1])]}}
+    buy_prices[coin_name_1][coin_name_2] = {'price': [float(coin1coin2_depth['asks'][0][0]), float(coin1coin2_depth['asks'][1][0])], 'volume': [float(coin1coin2_depth['asks'][0][1]), float(coin1coin2_depth['asks'][1][1])]}
+    if coin_name_2 in ['BNB', 'TUSD']:
+        buy_prices['ETH'] = {coin_name_2: {'price': [1/float(ethcoin2_depth['asks'][0][0]), 1/float(ethcoin2_depth['asks'][1][0])], 'volume': [float(ethcoin2_depth['asks'][0][1]), float(ethcoin2_depth['asks'][1][1])]}}        
+    else:
+        buy_prices['ETH'] = {coin_name_2: {'price': [float(ethcoin2_depth['asks'][0][0]), float(ethcoin2_depth['asks'][1][0])], 'volume': [float(ethcoin2_depth['asks'][0][1]), float(ethcoin2_depth['asks'][1][1])]}}
+
+    buy_prices['ETH'][coin_name_1] = {'price': [1/float(coin1eth_depth['bids'][0][0]), 1/float(coin1eth_depth['bids'][1][0])], 'volume': [float(coin1eth_depth['bids'][0][1]), float(coin1eth_depth['bids'][1][1])]}
+    if coin_name_2 in ['BNB', 'TUSD']:
+        buy_prices[coin_name_2] = {'ETH': {'price': [float(ethcoin2_depth['bids'][0][0]), float(ethcoin2_depth['bids'][1][0])], 'volume': [float(ethcoin2_depth['bids'][0][1]), float(ethcoin2_depth['bids'][1][1])]}}        
+    else:
+        buy_prices[coin_name_2] = {'ETH': {'price': [1/float(ethcoin2_depth['bids'][0][0]), 1/float(ethcoin2_depth['bids'][1][0])], 'volume': [float(ethcoin2_depth['bids'][0][1]), float(ethcoin2_depth['bids'][1][1])]}}
+    buy_prices[coin_name_2][coin_name_1] = {'price': [1/float(coin1coin2_depth['bids'][0][0]), 1/float(coin1coin2_depth['bids'][1][0])], 'volume': [float(coin1coin2_depth['bids'][0][1]), float(coin1coin2_depth['bids'][1][1])]}
+
+#    if coin_name_3:
+#        if coin_name_3 in ['BNB', 'TUSD']:
+#            buy_prices[coin_name_3] = {'ETH': {'price': [float(ethcoin3_depth['bids'][0][0]), float(ethcoin3_depth['bids'][1][0])], 'volume': [float(ethcoin3_depth['bids'][0][1]), float(ethcoin3_depth['bids'][1][1])]}}        
+#            buy_prices['ETH'][coin_name_3] = {'price': [1/float(ethcoin3_depth['asks'][0][0]), 1/float(ethcoin3_depth['asks'][1][0])], 'volume': [float(ethcoin3_depth['asks'][0][1]), float(ethcoin3_depth['asks'][1][1])]}        
+#        else:
+#            buy_prices[coin_name_3] = {'ETH': {'price': [1/float(ethcoin3_depth['bids'][0][0]), 1/float(ethcoin3_depth['bids'][1][0])], 'volume': [float(ethcoin3_depth['bids'][0][1]), float(ethcoin3_depth['bids'][1][1])]}}
+#            buy_prices['ETH'][coin_name_3] = {'price': [float(ethcoin3_depth['asks'][0][0]), float(ethcoin3_depth['asks'][1][0])], 'volume': [float(ethcoin3_depth['asks'][0][1]), float(ethcoin3_depth['asks'][1][1])]}        
+#
+#        buy_prices[coin_name_3][coin_name_2] = {'price': [1/float(coin2coin3_depth['bids'][0][0]), 1/float(coin2coin3_depth['bids'][1][0])], 'volume': [float(coin2coin3_depth['bids'][0][1]), float(coin2coin3_depth['bids'][1][1])]}         
+    return(buy_prices, eth_available)
+        
+
+def sim_tri(exchange_rates, swap_order, available):
+    """
+    ETH -> coin -> BTC -> ETH
+    """
+    
+#    coin_1, coin_2 = [coin2, coin1]
+#    exchange_rates = prices
+#    available = float(available_eth['free'])
+#    available = 0.18805411
+    
+    coin_1, coin_2 = swap_order
+
+    try:
+        exchange_hist = [('ETH', available)]
+        # 1 ETH -> x coin
+        coin_1_from_eth = available/exchange_rates[coin_1]['ETH']['price'][0]
+        if coin_1_from_eth <= exchange_rates[coin_1]['ETH']['volume'][0]:
+            coin_1_from_eth *= .999
+            exchange_hist.append((coin_1, coin_1_from_eth))
+        else:
+            raise Exception('Illiquid')
+            
+        # x coin -> y BTC
+        coin_2_from_coin_1 = coin_1_from_eth / exchange_rates[coin_2][coin_1]['price'][0]
+        if coin_1_from_eth <= exchange_rates[coin_2][coin_1]['volume'][0]:
+            coin_2_from_coin_1 *= .999
+            exchange_hist.append((coin_2, coin_2_from_coin_1))
+        else:
+            raise Exception('Illiquid')   
+     
+        # y BTC -> z ETH
+        eth_from_coin_2 = coin_2_from_coin_1 / exchange_rates['ETH'][coin_2]['price'][0]
+        if coin_2_from_coin_1 <= exchange_rates['ETH'][coin_2]['volume'][0]:
+            eth_from_coin_2 *= .999
+            exchange_hist.append(('ETH', eth_from_coin_2))
+        else:
+            raise Exception('Illiquid')   
+        
+        return(exchange_hist)
+    except:
+        return(False)
+        
+        
+def filter_potentials(psql): 
+#    psql = PSQL_2
+    
+    eth_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'ETH'"
+    eth_prices = pg_query(psql.client, eth_script)
+    eth_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    eth_conv = {k:float(v) for k,v in eth_prices[['sell_coin', 'open']].values}
+    
+    btc_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'BTC'"
+    btc_prices = pg_query(psql.client, btc_script)
+    btc_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    btc_conv = {k:float(v) for k,v in btc_prices[['sell_coin', 'open']].values}
+    
+    eth_btc = btc_conv['ETH']
+    btc_conv.pop('ETH')
+    
+    btc_traingle = {}
+    for coin_2_name, coin_2_eth_price in eth_conv.items():
+        if coin_2_name in btc_conv.keys():
+            coin_2_btc_price = btc_conv[coin_2_name]
+            btc_arb = ((1/coin_2_eth_price)*coin_2_btc_price)/eth_btc
+            btc_traingle[coin_2_name] = abs(1 - btc_arb)
+            
+    btc_traingle = sorted(btc_traingle.items(), key=operator.itemgetter(1), reverse = True)
+    btc_potentials = [('BTC', i[0], i[1]) for i in btc_traingle if i[1] >= .005]
+    
+    bnb_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'BNB'"
+    bnb_prices = pg_query(psql.client, bnb_script)
+    bnb_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    bnb_conv = {k:float(v) for k,v in bnb_prices[['sell_coin', 'open']].values}
+    
+    eth_bnb = 1/eth_conv['BNB']
+
+    bnb_traingle = {}
+    for coin_2_name, coin_2_eth_price in eth_conv.items():
+        if coin_2_name in bnb_conv.keys():
+            coin_2_bnb_price = bnb_conv[coin_2_name]
+            bnb_arb = ((1/coin_2_eth_price)*coin_2_bnb_price)/eth_bnb
+            bnb_traingle[coin_2_name] = abs(1 - bnb_arb)
+            
+    bnb_traingle = sorted(bnb_traingle.items(), key=operator.itemgetter(1), reverse = True)
+    bnb_potentials = [('BNB', i[0], i[1]) for i in bnb_traingle if i[1] >= .005]
+    
+    pax_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'PAX'"
+    pax_prices = pg_query(psql.client, pax_script)
+    pax_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    pax_conv = {k:float(v) for k,v in pax_prices[['sell_coin', 'open']].values}
+    
+    eth_pax = pax_conv['ETH']
+    pax_conv.pop('ETH')
+
+    pax_traingle = {}
+    for coin_2_name, coin_2_eth_price in eth_conv.items():
+        if coin_2_name in pax_conv.keys():
+            coin_2_pax_price = pax_conv[coin_2_name]
+            pax_arb = ((1/coin_2_eth_price)*coin_2_pax_price)/eth_pax
+            pax_traingle[coin_2_name] = abs(1 - pax_arb)
+            
+    pax_traingle = sorted(pax_traingle.items(), key=operator.itemgetter(1), reverse = True)
+    pax_potentials = [('PAX', i[0], i[1]) for i in pax_traingle if i[1] >= .005]
+    
+    tusd_script = "select ex_market_id, ex_time_id, open, sell_coin, buy_coin \
+    	from binance.exchanges ex \
+    	join binance.markets mk on mk.market_id = ex_market_id \
+    	where ex_time_id = (select max(t2.time_id) from binance.times t2) \
+    		and buy_coin = 'TUSD'"
+    tusd_prices = pg_query(psql.client, tusd_script)
+    tusd_prices.columns = ['market_id', 'time_id', 'open', 'sell_coin', 'buy_coin']
+    tusd_conv = {k:float(v) for k,v in tusd_prices[['sell_coin', 'open']].values}
+    
+    eth_tusd = 1/eth_conv['TUSD']
+
+    tusd_traingle = {}
+    for coin_2_name, coin_2_eth_price in eth_conv.items():
+        if coin_2_name in tusd_conv.keys():
+            coin_2_tusd_price = tusd_conv[coin_2_name]
+            tusd_arb = ((1/coin_2_eth_price)*coin_2_tusd_price)/eth_tusd
+            tusd_traingle[coin_2_name] = abs(1 - tusd_arb)
+            
+    tusd_traingle = sorted(tusd_traingle.items(), key=operator.itemgetter(1), reverse = True)
+    tusd_potentials = [('TUSD', i[0], i[1]) for i in tusd_traingle if i[1] >= .005]    
+    
+#    bnb_btc = btc_conv['BNB']
+#    bnb_btc_square = {}
+#    for coin_2_name, coin_2_eth_price in eth_conv.items():
+#        if coin_2_name in bnb_conv.keys() and coin_2_name in btc_conv.keys():
+#            coin_2_bnb_price = bnb_conv[coin_2_name]
+#            coin_2_btc_price = btc_conv[coin_2_name]
+#            bnb_btc_arb = eth_btc / coin_2_btc_price * coin_2_bnb_price / eth_bnb
+#            bnb_btc_square[coin_2_name] = abs(1 - bnb_btc_arb)
+#    bnb_btc_square = sorted(bnb_btc_square.items(), key=operator.itemgetter(1), reverse = True)
+#    bnb_btc_potentials = [(['BNB', 'BTC'], i[0], i[1]) for i in bnb_btc_square if i[1] >= .005]
+
+    _potentials = btc_potentials + bnb_potentials + pax_potentials + tusd_potentials# + bnb_btc_potentials
+
+    return(_potentials)
+
+def find_arb():
+    next_update = floor_dt(datetime.now(), timedelta(minutes=30))
+    dt_until_update = next_update - datetime.now()
+    seconds_before_update = dt_until_update.total_seconds()
+    PSQL_2 = db_connection('psql')
+    BINANCE_2 = binance_connection()
+    potentials = filter_potentials(PSQL_2)
+    while seconds_before_update > 0:
+        for coin2, coin1, _ in potentials: 
+            
+#            coin2,coin1,_ = potentials[-1]
+            prices, available_eth = cap_market(coin1, coin2, BINANCE_2)
+            forward_tri = sim_tri(prices, [coin2, coin1], float(available_eth['free']))
+            if forward_tri:
+                print('ETH -> %s -> %s -> ETH arbitrage: %.3f%%' % (coin2, coin1, 100 * (forward_tri[-1][-1] - forward_tri[0][-1])))
+                if forward_tri[-1][-1] > forward_tri[0][-1]:
+                    print(forward_tri)
+                    raise Exception('Found Profit!!!')
+            backward_tri = sim_tri(prices, [coin1, coin2], float(available_eth['free']))
+            if backward_tri:
+                print('ETH -> %s -> %s -> ETH arbitrage: %.3f%%' % (coin1, coin2, 100 * (backward_tri[-1][-1] - backward_tri[0][-1])))
+                if backward_tri[-1][-1] > backward_tri[0][-1]:
+                    print(backward_tri)
+                    raise Exception('Found Profit!!!')
+#        tm.sleep(15)
+        dt_until_update = next_update - datetime.now()
+        seconds_before_update = dt_until_update.total_seconds()     
+        
+    BINANCE_2 = None
+    PSQL_2.disconnect()
+    PSQL_2 = None
+    prices = None
+    coin1 = None
+    coin2 = None
+    forward_tri = None
+    backward_tri = None
+    next_update = None
+    dt_until_update = None
+    seconds_before_update = None
+            
+
 def update():
     PSQL = db_connection('psql')
     BINANCE = binance_connection()
     
     page = requests.get('https://info.binance.com/en/all')
     tree = html.fromstring(page.content)
-    COINS = [{'name':i, 'price':j} for i,j in zip(tree.xpath('//*[@id="__next"]/div/main/div/div/div/div[2]/div[1]/table/tbody/tr/td[2]/div/div/span[1]/text()')[:50], tree.xpath('//*[@id="__next"]/div/main/div/div/div/div[2]/div[1]/table/tbody/tr/td[3]/div/div/text()')[:50])]
+    COINS = [{'name':i, 'price':j} for i,j in zip(tree.xpath('//*[@id="__next"]/div/main/div/div/div/div[2]/div[1]/table/tbody/tr/td[2]/div/div/span[1]/text()'), tree.xpath('//*[@id="__next"]/div/main/div/div/div/div[2]/div[1]/table/tbody/tr/td[3]/div/div/text()'))]
           
          
     LAST_TIME = floor_dt(datetime.now(), timedelta(minutes=-30))
@@ -328,7 +574,7 @@ def update():
     
     pop_exchanges(PSQL, LAST_TIME, PRICES, MRKT_CONV, CURRENT_MKT_DATA, TIME_CONV, BINANCE)
     
-    pop_prices(PSQL, TIME_CONV, COINS)
+#    pop_prices(PSQL, TIME_CONV, COINS)
     
     PSQL.disconnect()
     PSQL = None
@@ -352,9 +598,14 @@ if __name__ == '__main__':
     if cli:
         update()
         while True:
-            next_update = floor_dt(datetime.now(), timedelta(minutes=30))
-            dt_until_update = next_update - datetime.now()
-            seconds_before_update = dt_until_update.total_seconds()
-            tm.sleep(seconds_before_update)
+            find_arb()
+                
+#            tm.sleep(seconds_before_update)
             update()
             
+            
+            
+
+       
+    
+    
